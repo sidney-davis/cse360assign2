fe0063664e4be520ebe6f5a0ecafd7bfd15a14e5
sidneydavis <sidneydavis77@gmail.com>
2019-02-23 12:01:20 -0700

    Committing the initial version.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,73 @@
+/* Name: Sidney Davis
+ * Class ID: 360
+ * Program Purpose: This program allows you to do basic calculator functions like addition, subtraction, multiplication and division.
+ */
+
+package cse360assign2;
+
+/**
+ * @author Sidney Davis
+ * This class consists of methods that represent calculator operations and manipulate the total based on the operation and parameters
+ * entered. 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Class Constructor
+	 * Initializes the total to 0
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method gets the total and returns zero.
+	 * @return Returns 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method currently has no use.
+	 * @param value An integer value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method currently has no use.
+	 * @param value An integer value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method currently has no use.
+	 * @param value An integer value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method currently has no use.
+	 * @param value An integer value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method returns a string of the history.
+	 * @return Returns an empty string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
